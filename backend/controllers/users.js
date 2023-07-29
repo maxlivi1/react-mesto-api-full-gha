e1ae1ffdf4b71481c1bac492b9cd95060689074b
Max Limanskii <maxlivi@yandex.ru>
2023-07-29 18:17:29 +0300

    refactor: изменить контроллер logout на сервере

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -66,7 +66,11 @@ const login = (req, res, next) => {
 
 const logout = (req, res, next) => {
   try {
-    res.clearCookie('jwt');
+    res.clearCookie('jwt', {
+      httpOnly: true,
+      sameSite: 'none',
+      secure: true,
+    });
     res.send({ message: 'Пользователь разлогинен.' });
   } catch (error) {
     next(error);
