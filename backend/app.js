8ed82f0315aa275f7b5678a30037ad87af03fbdd
Max Limanskii <maxlivi@yandex.ru>
2023-08-01 12:06:08 +0300

    refactor: исправить ограничение длины пароля

--- backend/app.js
+++ backend/app.js
@@ -60,7 +60,7 @@ app.post('/signin', celebrate({
 app.post('/signup', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
-    password: Joi.string().required().min(6),
+    password: Joi.string().required().min(5).max(100),
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
     avatar: Joi.string().pattern(URL_PATTERN),
